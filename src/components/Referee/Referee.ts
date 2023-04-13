b031372b0828cc0c844edf2165555bfb153e8384
Amien kurniawan <offdevamienk@gmail.com>
2023-04-13 07:14:59 +0700

    adding rule attacking pawn

--- src/components/Referee/Referee.ts
+++ src/components/Referee/Referee.ts
@@ -11,13 +11,22 @@ export default class Referee {
     }
   }
 
+  tileIsOccupiedByOpponent(movingX: number, movingY: number, boardState: Piece[], movingTeam: pieceTeam): boolean {
+    const boardOccupiedByOpponent = boardState.find((p) => p.x === movingX && p.y === movingY && p.team !== movingTeam);
+    if (boardOccupiedByOpponent) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   isValidMove(prevY: number, prevX: number, currY: number, currX: number, type: pieceType, team: pieceTeam, boardState: Piece[]) {
-    console.log("Referee is checking the move...")
-    console.log(`prev loc (x,y):(${prevX},${prevY})`)
-    console.log(`current loc (x,y):(${currX},${currY})`)
-    console.log(`Type piece:(${type})`)
-    console.log(`Type Team:(${team})`)
-    console.log('Board State : (', boardState, ')')
+    // console.log("Referee is checking the move...")
+    // console.log(`prev loc (x,y):(${prevX},${prevY})`)
+    // console.log(`current loc (x,y):(${currX},${currY})`)
+    // console.log(`Type piece:(${type})`)
+    // console.log(`Type Team:(${team})`)
+    // console.log('Board State : (', boardState, ')')
 
     // REFACTORING
 
@@ -25,6 +34,7 @@ export default class Referee {
       const specialRow = (team === pieceTeam.Our) ? 1 : 6;
       const pawnDirection = (team === pieceTeam.Our) ? 1 : -1;
 
+      // Movement Logic
       if (prevX === currX && prevY === specialRow && currY - prevY === 2 * pawnDirection) {
         if (!this.isOccupied(currY, currX, boardState) && !this.isOccupied(currY - pawnDirection, currX, boardState)) {
           console.log("valid move!")
@@ -36,87 +46,25 @@ export default class Referee {
           return true;
         }
       }
+
+      // Attack Logic
+      else if (currX - prevX === -1 && currY - prevY === pawnDirection) {
+        // Attack in upper or Bottom Left Corner
+        console.log('upper or bottom left')
+        if (this.tileIsOccupiedByOpponent(currX, currY, boardState, team)) {
+          console.log('we can attact the enemy!')
+        }
+      } else if (currX - prevX === 1 && currY - prevY === pawnDirection) {
+        // Attack in upper or Bottom Right Corner
+        console.log('upper or bottom right')
+        if (this.tileIsOccupiedByOpponent(currX, currY, boardState, team)) {
+          console.log('we can attact the enemy!')
+        }
+      }
     }
 
 
-    // if (type === pieceType.Pawn) {
-    //   const specialRow = (team === pieceTeam.Our) ? 1 : 6;
-    //   const pawnDirection = (team === pieceTeam.Our) ? 1 : -1;
 
-    //   if (prevY === specialRow) {
-    //     if (prevX === currX && currY - prevY === pawnDirection) {
-    //       if (!this.isOccupied(currY, currX, boardState)) {
-    //         console.log("valid move!")
-    //         return true;
-    //       }
-    //     } else if (prevX === currX && currY - prevY === 2 * pawnDirection) {
-    //       if (!this.isOccupied(currY, currX, boardState) && !this.isOccupied(currY - pawnDirection, currX, boardState)) {
-    //         console.log("valid move!")
-    //         return true;
-    //       }
-    //     }
-    //   } else {
-    //     if (prevX === currX && currY - prevY === pawnDirection) {
-    //       if (!this.isOccupied(currY, currX, boardState)) {
-    //         console.log("valid move~!")
-    //         return true;
-    //       }
-    //     }
-    //   }
-    // }
-
-
-    // if (type === pieceType.Pawn) {
-    //   if (team === pieceTeam.Our) {
-    //     if (prevY === 1) {
-
-    //       if (prevX === currX && (currY - prevY) === 2) {
-    //         if (!this.isOccupied(currY, currX, boardState) && !this.isOccupied(currY - 1, currX, boardState)) {
-    //           console.log("valid move!")
-    //           return true;
-    //         }
-    //       }
-
-    //       if (prevX === currX && (currY - prevY) === 1) {
-    //         if (!this.isOccupied(currY, currX, boardState)) {
-    //           console.log("valid move!")
-    //           return true;
-    //         }
-    //       }
-    //     } else {
-    //       if (prevX === currX && ((currY - prevY) === 1)) {
-    //         if (!this.isOccupied(currY, currX, boardState)) {
-    //           console.log("valid move!")
-    //           return true;
-    //         }
-    //       }
-    //     }
-    //   } else {
-    //     if (prevY === 6) {
-
-    //       if (prevX === currX && (currY - prevY) === -1) {
-    //         if (!this.isOccupied(currY, currX, boardState)) {
-    //           console.log("valid move~!")
-    //           return true;
-    //         }
-    //       }
-
-    //       if (prevX === currX && (currY - prevY) === -2) {
-    //         if (!this.isOccupied(currY, currX, boardState) && !this.isOccupied(currY + 1, currX, boardState)) {
-    //           console.log("valid move~!")
-    //           return true;
-    //         }
-    //       }
-    //     } else {
-    //       if (prevX === currX && ((currY - prevY) === -1)) {
-    //         if (!this.isOccupied(currY, currX, boardState)) {
-    //           console.log("valid move~!")
-    //           return true;
-    //         }
-    //       }
-    //     }
-    //   }
-    // }
     return false;
   }
 }
